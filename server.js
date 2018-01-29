f35c6e5f1d66a7c2efba45cba14f3e60fb295e6b
Yahor Simak <simak_yahor@mail.ru>
2018-01-29 23:43:00 +0300

    add config for express env

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoClient = require('mongodb').MongoClient;
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./app/routes')(app, {});
+
 const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+
+mongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+})
