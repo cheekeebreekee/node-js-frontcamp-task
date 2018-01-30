b79153125dd07e352ef3df3e4824ea0efb2fc756
Yahor Simak <simak_yahor@mail.ru>
2018-01-30 21:15:49 +0300

    add logging using morgan

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+const logger = require('morgan');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 8000;
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'app/access.log'), {flags: 'a'});
+app.use(logger('combined', {stream: accessLogStream}));
 
 app.use(bodyParser.urlencoded({extened:true}));
 
